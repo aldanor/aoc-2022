589a3a21c4e4bb394d7056b9ce4bbe1f91c5cdc1
Ivan Smirnov <i.s.smirnov@gmail.com>
2022-12-02 20:51:10 +0000

    Day 02: an even tighter 4-bit overlapping packing

--- src/day02/mod.rs
+++ src/day02/mod.rs
@@ -17,52 +17,48 @@ fn find_mapping() {
         for i in 0..9 {
             x[i] = (s[i].wrapping_mul(mul) >> shift) as u8;
         }
-        // we want to ensure no collisions between 3-bit values so we can pack the score map
-        for i in 0..8 {
-            for j in (i + 1)..9 {
-                if ((x[i] as i8) - (x[j] as i8)).abs() < 3 {
-                    continue 'outer;
-                }
+        let table1 = [4, 8, 3, 1, 5, 9, 7, 2, 6];
+        let table2 = [3, 4, 8, 1, 5, 9, 2, 6, 7];
+        let (mut map1, mut map2) = (0_u32, 0_u32);
+        for i in 0..9 {
+            map1 |= (table1[i]) << x[i];
+            map2 |= (table2[i]) << x[i];
+        }
+        // there WILL be overlaps and collisions, but let's see if we can find when there's none
+        for i in 0..9 {
+            if table1[i] != (map1 >> x[i]) & 0xf || table2[i] != (map2 >> x[i]) & 0xf {
+                continue 'outer;
             }
         }
-        // now we can pack all scores (sub 1 from all, and 2 from 9) into the u32 at given indices
-        let build_map = |table: [u8; 9], x: [u8; 9]| {
-            (0..9).fold(0_u32, |a, i| a | ((table[i] - 1).min(7) as u32) << x[i])
-        };
-        let map1 = build_map([4, 8, 3, 1, 5, 9, 7, 2, 6], x);
-        let map2 = build_map([3, 4, 8, 1, 5, 9, 2, 6, 7], x);
         println!("mul:   {:#08x}", mul);
         println!("shift: {}", shift);
-        println!("idx:   {:?}", x);
-        println!("'9':   {:#08x}", s[5]);
         println!("map1:  {:#08x}", map1);
         println!("map2:  {:#08x}", map2);
+        println!("idx:   {:?}", x);
         break;
     }
 }
 
 pub fn solve(s: &[u8], score_map: u32) -> u32 {
     /*
-    mul:   0x58bb8d84
+    mul:   0x54d672f3
     shift: 27
-    idx:   [24, 10, 28, 3, 21, 7, 15, 0, 18]
-    '9':   0xa5a2042
-    map1:  0x23971f81
-    map2:  0x72988f85
+    map1:  0xd024a357
+    map2:  0xe8259852
+    idx:   [11, 25, 8, 21, 4, 18, 0, 14, 29]
      */
     bytemuck::cast_slice::<_, u32>(s)
         .iter()
-        .map(|&x| ((score_map >> (x.wrapping_mul(0x58bb8d84) >> 27)) & 7) + (x == 0xa5a2042) as u32)
-        .sum::<u32>()
-        + (s.len() >> 2) as u32
+        .map(|&x| ((score_map >> (x.wrapping_mul(0x54d672f3) >> 27)) & 15))
+        .sum()
 }
 
 pub fn part1(s: &[u8]) -> u32 {
-    solve(s, 0x23971f81)
+    solve(s, 0xd024a357)
 }
 
 pub fn part2(s: &[u8]) -> u32 {
-    solve(s, 0x72988f85)
+    solve(s, 0xe8259852)
 }
 
 #[test]
