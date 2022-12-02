a094f1c0a512fee97382c9a2d257eb4b6b8d7c66
Ivan Smirnov <i.s.smirnov@gmail.com>
2022-12-02 16:58:04 +0000

    Day 02: speedup, u32 only, pack scores tighter

--- src/day02/mod.rs
+++ src/day02/mod.rs
@@ -5,36 +5,64 @@ pub fn input() -> &'static [u8] {
     include_bytes_align_as!(u32, "input.txt")
 }
 
-/*
-using mask of 0x14b6cc35 and shift of 22 for each "(A|B|C) (X|Y|Z)\n" entry,
-we can wrapping-mul and take the lowest 4 bits to get the outcome index (0-8):
-            part1   part2
-A X => 0    4       3
-A Y => 1    8       4
-A Z => 2    3       8
-B X => 3    1       1
-B Y => 4    5       5
-B Z => 5    9       9
-C X => 6    7       2
-C Y => 7    2       6
-C Z => 8    6       7
- */
+#[allow(dead_code)]
+fn find_mapping() {
+    // we can run this once to generate the magic numbers and it will work for all inputs
+    let s = bytemuck::cast_slice::<_, u32>(include_bytes_align_as!(u32, "9.txt"));
+    let mut r = rand::thread_rng();
+    'outer: loop {
+        let mul: u32 = rand::Rng::gen(&mut r);
+        let shift = 27; // 32 - 27 = 5 bits left, 2^5 = 32 for shifts around the u32
+        let mut x = [0_u8; 9];
+        for i in 0..9 {
+            x[i] = (s[i].wrapping_mul(mul) >> shift) as u8;
+        }
+        // we want to ensure no collisions between 3-bit values so we can pack the score map
+        for i in 0..8 {
+            for j in (i + 1)..9 {
+                if ((x[i] as i8) - (x[j] as i8)).abs() < 3 {
+                    continue 'outer;
+                }
+            }
+        }
+        // now we can pack all scores (sub 1 from all, and 2 from 9) into the u32 at given indices
+        let build_map = |table: [u8; 9], x: [u8; 9]| {
+            (0..9).fold(0_u32, |a, i| a | ((table[i] - 1).min(7) as u32) << x[i])
+        };
+        let map1 = build_map([4, 8, 3, 1, 5, 9, 7, 2, 6], x);
+        let map2 = build_map([3, 4, 8, 1, 5, 9, 2, 6, 7], x);
+        println!("mul:   {:#08x}", mul);
+        println!("shift: {}", shift);
+        println!("idx:   {:?}", x);
+        println!("'9':   {:#08x}", s[5]);
+        println!("map1:  {:#08x}", map1);
+        println!("map2:  {:#08x}", map2);
+        break;
+    }
+}
 
-pub fn solve(s: &[u8], score_map: [u8; 9]) -> u32 {
-    let score_map = score_map.iter().rev().fold(0, |acc, &x| (acc << 4) | x as u64);
+pub fn solve(s: &[u8], score_map: u32) -> u32 {
+    /*
+    mul:   0x58bb8d84
+    shift: 27
+    idx:   [24, 10, 28, 3, 21, 7, 15, 0, 18]
+    '9':   0xa5a2042
+    map1:  0x23971f81
+    map2:  0x72988f85
+     */
     bytemuck::cast_slice::<_, u32>(s)
         .iter()
-        // extract 0..=8 index, multiply by 4 to get the shift, shift right and get the lower 4 bits
-        .map(|&x| ((score_map >> (((x.wrapping_mul(0x14b6cc35) >> 22) & 15) << 2)) & 15) as u32)
-        .sum()
+        .map(|&x| ((score_map >> (x.wrapping_mul(0x58bb8d84) >> 27)) & 7) + (x == 0xa5a2042) as u32)
+        .sum::<u32>()
+        + (s.len() >> 2) as u32
 }
 
 pub fn part1(s: &[u8]) -> u32 {
-    solve(s, [4, 8, 3, 1, 5, 9, 7, 2, 6])
+    solve(s, 0x23971f81)
 }
 
 pub fn part2(s: &[u8]) -> u32 {
-    solve(s, [3, 4, 8, 1, 5, 9, 2, 6, 7])
+    solve(s, 0x72988f85)
 }
 
 #[test]
