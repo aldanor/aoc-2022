fb90e7f9a23187980e86b8e323e326eb7649909b
Ivan Smirnov <i.s.smirnov@gmail.com>
2022-12-03 12:49:01 +0000

    Day 03

--- src/day03/mod.rs
+++ src/day03/mod.rs
@@ -5,20 +5,47 @@ pub fn input() -> &'static [u8] {
     include_bytes!("input.txt")
 }
 
-pub fn part1(_s: &[u8]) -> u16 {
-    0
+fn char_to_score(c: u8) -> u8 {
+    if c >= b'a' {
+        c - b'a' + 1
+    } else {
+        c - b'A' + 27
+    }
 }
 
-pub fn part2(_s: &[u8]) -> u16 {
-    0
+pub fn part1(mut s: &[u8]) -> u32 {
+    let mut total = 0;
+    while s.len() > 1 {
+        let n = s.memchr(b'\n');
+        let line = &s[..n];
+        let left = line[..n >> 1].iter().fold(0u64, |acc, &c| acc | (1 << (c - b'A')));
+        let right = line[n >> 1..].iter().fold(0u64, |acc, &c| acc | (1 << (c - b'A')));
+        total += char_to_score((left & right).trailing_zeros() as u8 + b'A') as u32;
+        s = &s[(n + 1)..];
+    }
+    total
+}
+
+pub fn part2(mut s: &[u8]) -> u32 {
+    let mut total = 0;
+    while s.len() > 1 {
+        let mut badge = u64::MAX;
+        for _ in 0..3 {
+            let n = s.memchr(b'\n');
+            badge &= s[..n].iter().fold(0u64, |acc, &c| acc | (1 << (c - b'A')));
+            s = &s[(n + 1)..];
+        }
+        total += char_to_score(badge.trailing_zeros() as u8 + b'A') as u32;
+    }
+    total
 }
 
 #[test]
 fn test_part1() {
-    assert_eq!(part1(input()), 0);
+    assert_eq!(part1(input()), 8349);
 }
 
 #[test]
 fn test_part2() {
-    assert_eq!(part2(input()), 0);
+    assert_eq!(part2(input()), 2681);
 }
