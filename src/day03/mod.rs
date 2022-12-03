aa703cd3817a44676eaecc3013e6dc5e19495225
Ivan Smirnov <i.s.smirnov@gmail.com>
2022-12-03 13:48:52 +0000

    Day 03: slight optimization

--- src/day03/mod.rs
+++ src/day03/mod.rs
@@ -15,13 +15,21 @@ fn char_to_score(c: u8) -> u8 {
 
 pub fn part1(mut s: &[u8]) -> u32 {
     let mut total = 0;
-    while s.len() > 1 {
+    'outer: while s.len() > 1 {
         let n = s.memchr(b'\n');
         let line = &s[..n];
-        let left = line[..n >> 1].iter().fold(0u64, |acc, &c| acc | (1 << (c - b'A')));
-        let right = line[n >> 1..].iter().fold(0u64, |acc, &c| acc | (1 << (c - b'A')));
-        total += char_to_score((left & right).trailing_zeros() as u8 + b'A') as u32;
         s = &s[(n + 1)..];
+        let (line_r, line_l) = line.split_at(n >> 1);
+        let (mut left, mut right) = (0u64, 0u64);
+        for i in 0..(n >> 1) {
+            left |= 1u64 << (line_l[i] - 64); // 64 is faster (on ARM, shifts are 6-bit masked)
+            right |= 1u64 << (line_r[i] - 64);
+            let cross = left & right;
+            if cross != 0 {
+                total += char_to_score(cross.trailing_zeros() as u8 | 64) as u32;
+                continue 'outer;
+            }
+        }
     }
     total
 }
@@ -32,10 +40,10 @@ pub fn part2(mut s: &[u8]) -> u32 {
         let mut badge = u64::MAX;
         for _ in 0..3 {
             let n = s.memchr(b'\n');
-            badge &= s[..n].iter().fold(0u64, |acc, &c| acc | (1 << (c - b'A')));
+            badge &= s[..n].iter().fold(0u64, |acc, &c| acc | (1 << (c - 64)));
             s = &s[(n + 1)..];
         }
-        total += char_to_score(badge.trailing_zeros() as u8 + b'A') as u32;
+        total += char_to_score(badge.trailing_zeros() as u8 | 64) as u32;
     }
     total
 }
